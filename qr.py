86824f43c9affc46c10758100fc74c337f8739ba
Ashref G <126174609+AshGGX@users.noreply.github.com>
2023-05-11 19:20:25 +0100

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,14 +1,19 @@
 import qrcode
-
-def makeqr(text):
-    with open(text,'r') as f :
-        x = f.read()
-    qr = qrcode.QRCode(version = 40,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=20,border=1)
-    qr.add_data(x)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color = "black", back_color="white")
-    img.save(("qrv40.png"))
-    img.show()
+from typing import Union
+def makeqr(text : str) -> Union [int,tuple] :
+    try:
+        with open(text,'r') as f :
+            x = f.read()            
+        qr = qrcode.QRCode(version = 40,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=20,border=1)
+        qr.add_data(x)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color = "black", back_color="white")
+        img.save(("qrv40.png"))
+        img.show()
+        return 1
+    
+    except Exception as e :
+        return (0,e)
 
 if __name__ == '__main__':
     makeqr('test1')
