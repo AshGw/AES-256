9b471e1cb659c2c756f1343d8106f449ac3da58c
Ashref G <126174609+AshGw@users.noreply.github.com>
2023-05-21 14:10:42 +0100

    Update Ash.py

--- Ash.py
+++ Ash.py
@@ -1,24 +1,49 @@
 
 ''' STILL IN THE WORKS '''
-
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
+import bcrypt
 import os
 import base64
 
 class Ash:
-    def __init__(self, message: str, key: str):
+    def __init__(self, message: str, mainkey: str):
         self.message = message
-        self.key = bytes.fromhex(key)
-        self.encKey = self.key[:32]
+        self.mainkey = mainkey
+        self.salt = os.urandom(16)
+        self.pepper = os.urandom(16)
+        self.iterations = 500
+        self.encKey = self.derkey1(self.mainkey, self.salt, self.iterations)
+        self.hmac_k = self.derkey2(self.mainkey,self.pepper,self.iterations)
+        
+    @staticmethod
+    def derkey1(mainkey: str, salt: bytes, iterations: int) -> bytes:
+        hmac_k = bcrypt.kdf(
+            password=mainkey.encode('UTF-8'),
+            salt=salt,
+            desired_key_bytes=32,
+            rounds=iterations
+        )
+        return hmac_k
+
+    @staticmethod
+    def derkey2(mainkey: str, pepper: bytes, iterations: int) -> bytes:
+        encKey = bcrypt.kdf(
+            password = mainkey.encode('UTF-8'),
+            salt = pepper,
+            desired_key_bytes = 32,
+            rounds= iterations
+        )
+        return encKey
 
     @staticmethod
     def genkey():
         return os.urandom(64).hex()
-
-    def iv(self):
+    
+    @staticmethod
+    def iv():
         return os.urandom(16)
     def mode(self):
         return modes.CBC(self.iv())
@@ -38,13 +63,24 @@ class Ash:
         return self.cipher_encryptor().update(self.padded_message()) + self.cipher_encryptor().finalize()
 
     def HMAC(self):
-        h = self.key[32:]
-        h = hmac.HMAC(h, hashes.SHA3_512())
+        h = self.hmac_k
+        h = hmac.HMAC(h, hashes.SHA512())
         h.update(self.ciphertext())
         return h.finalize()
 
     def combined_output(self):
-        return  self.HMAC() + self.iv() + self.ciphertext()
+        return  self.HMAC() + self.iv() + self.salt + self.pepper + self.ciphertext()
 
     def encrypt(self):
         return base64.urlsafe_b64encode(self.combined_output()).decode('UTF-8')
+
+if __name__ == '__main__':
+    
+    k512 = '818b5e3bb5a19e32cf3338c82f94015817bcc605f6ad0025840b3eb64853a2df'
+    testmessage = 'Hello World !'
+
+    a = Ash(testmessage, k512)
+    b = a.encrypt()
+    print(b)
+    print(b.__len__())
+    print(len(a.encKey))
