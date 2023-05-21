dd4f1009a0701c6b1fc39bba73e4a7b3a504bec6
Ashref G <126174609+AshGw@users.noreply.github.com>
2023-05-21 22:51:35 +0100

    Update Ash.py

--- Ash.py
+++ Ash.py
@@ -12,12 +12,12 @@ class Ash:
     def __init__(self, message: str, mainkey: str):
         self.message = message
         self.mainkey = mainkey
+        self.iv = os.urandom(16)
         self.salt = os.urandom(16)
         self.pepper = os.urandom(16)
-        self.iterations = 500
+        self.iterations = 100
         self.encKey = self.derkey1(self.mainkey, self.salt, self.iterations)
         self.hmac_k = self.derkey2(self.mainkey,self.pepper,self.iterations)
-        
     @staticmethod
     def derkey1(mainkey: str, salt: bytes, iterations: int) -> bytes:
         hmac_k = bcrypt.kdf(
@@ -41,24 +41,19 @@ class Ash:
     @staticmethod
     def genkey():
         return os.urandom(64).hex()
-    
-    @staticmethod
-    def iv():
-        return os.urandom(16)
-    def mode(self):
-        return modes.CBC(self.iv())
 
-    def padded_message(self):
-        padder = padding.PKCS7(128).padder()
-        return padder.update(self.message.encode('UTF-8')) + padder.finalize()
+    def mode(self):
+        return modes.CBC(self.iv)
 
     def cipher(self):
-        cipher = Cipher(algorithms.AES(self.encKey), self.mode(), backend=default_backend())
-        return cipher
+        return Cipher(algorithms.AES(key=self.encKey),mode=self.mode(), backend=default_backend())
 
     def cipher_encryptor(self):
         return self.cipher().encryptor()
 
+    def padded_message(self):
+        padder = padding.PKCS7(128).padder()
+        return padder.update(self.message.encode('UTF-8')) + padder.finalize()
     def ciphertext(self):
         return self.cipher_encryptor().update(self.padded_message()) + self.cipher_encryptor().finalize()
 
@@ -68,19 +63,32 @@ class Ash:
         h.update(self.ciphertext())
         return h.finalize()
 
-    def combined_output(self):
-        return  self.HMAC() + self.iv() + self.salt + self.pepper + self.ciphertext()
+    def combined_output(self)->bytes:
+        return  self.HMAC() + self.iv + self.salt + self.pepper + self.ciphertext()
 
-    def encrypt(self):
+    def encrypt(self)->str:
         return base64.urlsafe_b64encode(self.combined_output()).decode('UTF-8')
 
 if __name__ == '__main__':
-    
     k512 = '818b5e3bb5a19e32cf3338c82f94015817bcc605f6ad0025840b3eb64853a2df'
     testmessage = 'Hello World !'
 
     a = Ash(testmessage, k512)
-    b = a.encrypt()
+    b = a.combined_output()
+    c = a.encrypt()
     print(b)
-    print(b.__len__())
-    print(len(a.encKey))
+    print(a.HMAC().__len__())
+    print(a.HMAC())
+    print(b[:64])
+    print(a.iv.__len__())
+    print(a.iv)
+    print(b[64:80])
+    print(a.salt.__len__())
+    print(a.salt)
+    print(b[80:96])
+    print(a.pepper.__len__())
+    print(a.pepper)
+    print(b[96:112])
+    print(a.ciphertext().__len__())
+    print(a.ciphertext())
+    print(b[112:])
