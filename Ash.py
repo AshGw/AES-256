67e5e5bcd6942e431ad2ac663ed2294a5af8928b
Ashref G <126174609+AshGw@users.noreply.github.com>
2023-05-23 13:16:11 +0100

    Update Ash.py

--- Ash.py
+++ Ash.py
@@ -9,7 +9,7 @@ import os
 import base64
 
 class Ash:
-    def __init__(self, message: str, mainkey: str):
+    def __init__(self, message: str, mainkey: str)->None:
         self.message = message
         self.mainkey = mainkey
         self.iv = os.urandom(16)
@@ -21,26 +21,26 @@ class Ash:
         
     @staticmethod
     def derkey1(mainkey: str, salt: bytes, iterations: int) -> bytes:
-        hmac_k = bcrypt.kdf(
+        encKey = bcrypt.kdf(
             password=mainkey.encode('UTF-8'),
             salt=salt,
             desired_key_bytes=32,
             rounds=iterations
         )
-        return hmac_k
+        return encKey
 
     @staticmethod
     def derkey2(mainkey: str, pepper: bytes, iterations: int) -> bytes:
-        encKey = bcrypt.kdf(
+        hmac_k = bcrypt.kdf(
             password = mainkey.encode('UTF-8'),
             salt = pepper,
             desired_key_bytes = 32,
             rounds= iterations
         )
-        return encKey
+        return hmac_k
 
     @staticmethod
-    def genkey():
+    def genMainkey():
         return os.urandom(64).hex()
 
     def mode(self):
@@ -52,9 +52,10 @@ class Ash:
     def cipher_encryptor(self):
         return self.cipher().encryptor()
 
-    def padded_message(self):
+    def padded_message(self)->bytes:
         padder = padding.PKCS7(128).padder()
         return padder.update(self.message.encode('UTF-8')) + padder.finalize()
+    
     def ciphertext(self):
         return self.cipher_encryptor().update(self.padded_message()) + self.cipher_encryptor().finalize()
 
@@ -69,27 +70,3 @@ class Ash:
 
     def encrypt(self)->str:
         return base64.urlsafe_b64encode(self.combined_output()).decode('UTF-8')
-
-if __name__ == '__main__':
-    k512 = '818b5e3bb5a19e32cf3338c82f94015817bcc605f6ad0025840b3eb64853a2df'
-    testmessage = 'Hello World !'
-
-    a = Ash(testmessage, k512)
-    b = a.combined_output()
-    c = a.encrypt()
-    print(b)
-    print(a.HMAC().__len__())
-    print(a.HMAC())
-    print(b[:64])
-    print(a.iv.__len__())
-    print(a.iv)
-    print(b[64:80])
-    print(a.salt.__len__())
-    print(a.salt)
-    print(b[80:96])
-    print(a.pepper.__len__())
-    print(a.pepper)
-    print(b[96:112])
-    print(a.ciphertext().__len__())
-    print(a.ciphertext())
-    print(b[112:])
