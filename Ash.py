84d8e72c591e00a1d9a6f9c5686c9cd5d3d162e9
Ashref G <126174609+AshGw@users.noreply.github.com>
2023-05-23 13:23:50 +0100

    Update Ash.py

--- Ash.py
+++ Ash.py
@@ -8,7 +8,7 @@ import bcrypt
 import os
 import base64
 
-class Ash:
+class Enc:
     def __init__(self, message: str, mainkey: str)->None:
         self.message = message
         self.mainkey = mainkey
@@ -70,3 +70,58 @@ class Ash:
 
     def encrypt(self)->str:
         return base64.urlsafe_b64encode(self.combined_output()).decode('UTF-8')
+    
+ class Dec():
+    def __init__(self,message : bytes , key : str):
+        self.message = message
+        self.key = key
+        self.rec_hmac = self.message[:64]
+        self.rec_iv = self.message[64:80]
+        self.rec_salt = self.message[80:96]
+        self.rec_pepper = self.message[96:112]
+        self.rec_ciphertext = self.message[112:]
+        self.iterations = 100
+        self.decKey = self.derkey2(self.key,self.rec_salt,self.iterations)
+        self.hmac_k = self.derkey1(self.key, self.rec_pepper, self.iterations)
+
+    @staticmethod
+    def derkey1(mainkey: str, salt: bytes, iterations: int) -> bytes:
+        encKey = bcrypt.kdf(
+            password=mainkey.encode('UTF-8'),
+            salt=salt,
+            desired_key_bytes=32,
+            rounds=iterations
+        )
+        return encKey
+
+    @staticmethod
+    def derkey2(mainkey: str, pepper: bytes, iterations: int) -> bytes:
+        hmac_k = bcrypt.kdf(
+            password=mainkey.encode('UTF-8'),
+            salt=pepper,
+            desired_key_bytes=32,
+            rounds=iterations
+        )
+        return hmac_k
+
+
+    def mode(self):
+        return modes.CBC(self.rec_iv)
+
+    def cipher(self):
+        return Cipher(algorithms.AES(key=self.decKey),mode=self.mode(),backend=default_backend())
+
+    def cipher_decryptor(self):
+        return self.cipher().decryptor()
+
+    def unpadded_message(self):
+        # unpadder = padding.PKCS7(128).unpadder()
+        # return unpadder.update(self.rec_ciphertext) + unpadder.finalize()
+        return self.rec_ciphertext
+
+    def palaintext_bytes(self)->bytes:
+        return self.cipher_decryptor().update(self.unpadded_message()) + self.cipher_decryptor().finalize()
+
+    def decrypt(self)->str:
+        return (self.palaintext_bytes().decode('UTF-8'))
+
