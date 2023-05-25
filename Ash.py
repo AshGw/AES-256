470d78b3e70e95651a497caddfc8d18e548bb854
Ashref Gw <126174609+AshGw@users.noreply.github.com>
2023-05-25 19:51:38 +0100

    Phase II testing finsihed for both Enc and Dec

--- Ash.py
+++ Ash.py
@@ -14,6 +14,7 @@ import time
 
 class IterationsOutofRangeError(Exception):
     pass
+
 class Enc:
     def __init__(self, message: Union[str, bytes], mainkey: str) -> None:
         if isinstance(message, str):
@@ -89,6 +90,10 @@ class Enc:
 
     
     
+
+class IterationsOuttaRangeError(Exception):
+    pass
+
 class Dec():
     def __init__(self, message: bytes, key: str):
         self.message = message
@@ -98,9 +103,11 @@ class Dec():
         self.rec_salt = self.message[80:96]
         self.rec_pepper = self.message[96:112]
         self.rec_ciphertext = self.message[112:]
-        self.iterations = 100
-        self.decKey = self.derkey2(self.key, self.rec_salt, self.iterations)
-        self.hmac_k = self.derkey1(self.key, self.rec_pepper, self.iterations)
+        self.rec_iterations = struct.unpack('!I', self.message[-4:])[0]
+        if self.rec_iterations < 50 or self.rec_iterations > 100000:
+            raise IterationsOuttaRangeError
+        self.decKey = self.derkey2(self.key, self.rec_salt, self.rec_iterations)
+        self.hmac_k = self.derkey1(self.key, self.rec_pepper, self.rec_iterations)
 
     @staticmethod
     def derkey1(mainkey: str, salt: bytes, iterations: int) -> bytes:
@@ -143,6 +150,7 @@ class Dec():
 
     def decToStr(self) -> str:
         return (self.unpadded_m().decode('UTF-8'))
+
     
     
 
@@ -171,11 +179,10 @@ if __name__ == '__main__':
     t2 = time.perf_counter()
     print(t2-t1)
     
-    ''' Now For The Decryption Testing Phase I '''
+    ''' Now For The Decryption Testing Phase II '''
     
-    rec_mess = b'3\x8c\xfe\xc6\x9a\xce\xd3\xbc\r\x89\xda\x0b\xf2\x8d(\x7f\x05\xcf\x03%\x8e\t"h\x8a7\xf0C\xb3\xdb\xbf\xc6wP\xac\x14\\\x10^\xddw\x84\xf8(?o"\x17\x84\x8e\x0c\xa5n+\x0eo\xe9Y\xf5\x16}\x13"d\xeb\x023R{\xfa\xda\xe2\x1a\xf3\xdd\x83Cur\x022\x06\x96(t\x16<\xf0\xea\xe8\x8d\x83z9y"\x14\xfb\xa5%\x92\xa8\x0eU1\xb0\xb3\xf0\ru\x94\t\xe0\x8c`\xce\xfe \xbbC\x8b \x9a\xcb6\xd6\x90\xd5\x96\x8d?\x84"\xff\xf7O=\xfex\x99#Y\xb0:'
+    rec_mess_ite =b'\xf1\xdc\xd0f\xc3\xc7\x1e\xe1w#\x08Q\x7f\x16\xd87\x1b8k\x1f\xa7dj\xbfb\xee<\xff\xef\x00\xce\xa3\xd8jv\xe9\x9a\n\\\xc0\x13\xd4\xff]\x92\x10\x82r\x91XA\xa5\x08[ N\x16\xd3\x9e\xdc\x8d1yM\xca\xe8G\xecbkP\xa8\xae\x01\xd3\xee\x1bcL,S\xefV\xe7cI\x80\xf4\xf2\x0c\xaf\r\xa6\x82zLj\x1fu\xea/\x13\x04\x07\xd0\xf7\x81@\xf4\xfd\xb5\xcb\tE\xda\xa6\xd14F\xd53\xf7\xc0\x034\x9c\x00\xb7\x94\xdd0\xe9\xf0\x12\xc7cD\xab\x9c\xb9\xbe@\xfd\r\x00\x00\x002'
     key = '818b5e3bb5a19e32cf3338c82f94015817bcc605f6ad0025840b3eb64853a2df'
-    ins = Dec(rec_mess,key)
-    print(ins.decToStr())
-    
+    ins = Dec(rec_mess_ite,key)
+    print(ins.decToBytes())
 
