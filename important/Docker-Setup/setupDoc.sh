cb0c762e49fd415f9e27d986a4c84064a125be19
AshrefGw <AshGw@users.noreply.github.com>
2023-06-10 19:13:08 +0100

    Added Docker Configs

---
+++ important/Docker-Setup/setupDoc.sh
@@ -0,0 +1,12 @@
+apt update -y
+apt install -y curl
+apt install -y git
+apt install -y python3
+apt install -y python3-pip
+pip3 install -y virtualenv
+virtualenv vvv
+source vvv/bin/activate
+echo "tzdata tzdata/Areas select America" | debconf-set-selections
+echo "tzdata tzdata/Zones/America select Denver" | debconf-set-selections
+apt install -y python3-tk
+curl -sSfL https://raw.githubusercontent.com/AshGw/AES-256/main/important/setup.sh | bash
