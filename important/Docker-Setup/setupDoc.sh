7f991aeafc0c14557a8deb5b57dd50619327d9ed
AshrefGw <AshGw@users.noreply.github.com>
2023-06-10 22:33:34 +0100

    updated

--- important/Docker-Setup/setupDoc.sh
+++ important/Docker-Setup/setupDoc.sh
@@ -1,12 +1,10 @@
-apt update -y
-apt install -y curl
-apt install -y git
-apt install -y python3
-apt install -y python3-pip
+apt-get update -y
+apt-get install -y curl
+apt-get install -y git
+apt-get install -y python3
+apt-get install -y python3-pip
+pip3 install --upgrade pip
 pip3 install virtualenv
 virtualenv vvv
-source vvv/bin/activate
-echo "tzdata tzdata/Areas select America" | debconf-set-selections
-echo "tzdata tzdata/Zones/America select Denver" | debconf-set-selections
-apt install -y python3-tk
+source vvv/bin/activatec
 curl -sSfL https://raw.githubusercontent.com/AshGw/AES-256/main/important/setup.sh | bash
