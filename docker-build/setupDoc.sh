c1e7ba4164f5590ef0d50e7e2d95a5b3bf11484d
AshrefGw <AshGw@users.noreply.github.com>
2023-06-11 09:41:38 +0100

    settings

--- docker-build/setupDoc.sh
+++ docker-build/setupDoc.sh
@@ -1,10 +1,8 @@
 apt-get update -y
 apt-get install -y curl
 apt-get install -y git
-apt-get install -y python3
-apt-get install -y python3-pip
-pip3 install --upgrade pip
-pip3 install virtualenv
+pip install --upgrade pip
+pip install virtualenv
 virtualenv vvv
 source vvv/bin/activatec
 curl -sSfL https://raw.githubusercontent.com/AshGw/AES-256/main/important/setup.sh | bash
