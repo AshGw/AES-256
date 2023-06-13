803bd1971ab0ed67bf82d572d9e875e523fb1f4e
AshrefGw <AshGw@users.noreply.github.com>
2023-06-13 18:45:53 +0100

    Added

--- docker-build/env-setup.sh
+++ docker-build/env-setup.sh
@@ -1,7 +1,7 @@
+RUN apt-get update -y
 apt-get install -y curl
 apt-get install -y git
 pip install --upgrade pip
 pip install virtualenv
 virtualenv vvv
-source vvv/bin/activatec
-curl -sSfL https://raw.githubusercontent.com/AshGw/AES-256/main/important/setup.sh | bash
+source vvv/bin/activate
