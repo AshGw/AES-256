e5562db038d5755c12890cc22eecc8978bc13c2d
Ashref Gwader <126174609+AshGw@users.noreply.github.com>
2023-06-17 17:16:26 +0100

    Update

--- important/setup.sh
+++ important/setup.sh
@@ -2,6 +2,5 @@ git clone https://github.com/AshGw/AES-256.git
 cd AES-256
 pip install --upgrade pip
 pip install -r important/requirements.txt
-python setup.py develop
-cd src
+cd AshCrypt
 python -m CliCrypt
