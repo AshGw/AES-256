010493bfdd2036065e69e399d1f4f4955bae1aa2
AshrefGw <AshGw@users.noreply.github.com>
2023-06-17 17:27:31 +0100

    refactor

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-__all__ = ['AshCrypt','unittests']
+__all__ = ['AshCrypt', 'unittestsAC']
--- important/setup.sh
+++ important/setup.sh
@@ -2,5 +2,6 @@ git clone https://github.com/AshGw/AES-256.git
 cd AES-256
 pip install --upgrade pip
 pip install -r important/requirements.txt
+python setup.py develop
 cd AshCrypt
 python -m CliCrypt
