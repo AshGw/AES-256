5124ded262ac9cf833be7ee39861e1bdc90d2812
AshrefGw <AshGw@users.noreply.github.com>
2023-06-26 16:25:00 +0100

    Added content by id function

--- AshCrypt/AshDatabase.py
+++ AshCrypt/AshDatabase.py
@@ -142,6 +142,27 @@ class Database():
             except sqlite3.Error as e:
                 return (0, e)
 
+    def content_by_id(self, id : int ,optional_tablename=None):
+        if optional_tablename is None:
+            try:
+                with self.conn:
+                    self.c.execute(f'SELECT * FROM {self.tablename} WHERE ID = ? ',(id,))
+                    for row in self.c.fetchall():
+                        yield row
+
+            except sqlite3.Error as e:
+                return (0.0, e)
+
+        else:
+            try:
+                with self.conn:
+                    self.c.execute(f'SELECT * FROM {optional_tablename} WHERE ID = ? ',(id,))
+                    for row in self.c.fetchall():
+                        yield row
+
+            except sqlite3.Error as e:
+                return (0, e)
+
     def show_contents(self, *optional_tablenames):
         if optional_tablenames:
             try:
@@ -226,10 +247,13 @@ class Database():
 if __name__ == '__main__':
     conn = Database('post_test.db')
     conn.addtable()
-    key = '#5482A'
+
+    key = '#5482AF'
     conn.insert('My Accounts','some encrypted content of bytes or strings',key)
     for e in conn.show_tables():
         print(e)
     print(conn.size)
-    query1 = 'SELECT COUNT(*) AS cc ,content FROM Classified WHERE key = "#5482A" ORDER BY cc DESC '
+    query1 = 'SELECT COUNT(*) AS cc ,content FROM Classified WHERE key = "#5482AF" ORDER BY cc DESC '
     print(conn.query(query1))
+    for e in conn.content_by_id(1):
+        print(e)
