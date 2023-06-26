78e7ae00a47c9b5dde3619a490d54fe1e727df4b
AshrefGw <AshGw@users.noreply.github.com>
2023-06-26 01:38:31 +0100

    Done

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
-__all__ = ['AshCrypt', 'unittestsAC']
+import warnings
+import sys
+
+__all__ = ['AshCrypt']
+__version__ = "1.1.6"
+__author__ = "Ashref Gwader"
+
+if sys.version_info.major <= 3 and sys.version_info.minor <= 11 or sys.version_info.major < 3:
+    warnings.warn(
+        message='''
+            This Python version is no longer supported by the Python team nor is it supported by this Project
+            Python 3.7 or newer is required for this library to work properly !
+        ''',
+        stacklevel = 2
+    )
