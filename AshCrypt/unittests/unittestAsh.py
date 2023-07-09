b9d62ac54257849ccd5dc0e6ecae7a7316f87ab4
AshrefGw <AshGw@users.noreply.github.com>
2023-07-09 23:33:52 +0100

    organized

--- AshCrypt/unittests/unittestAsh.py
+++ AshCrypt/unittests/unittestAsh.py
@@ -2,6 +2,7 @@ import unittest
 import struct
 import AshCrypt.Ash as Ash
 
+
 class AshModuleTesting(unittest.TestCase):
     def setUp(self) -> None:
         self.message1 = 'Hello there testing if it works'
@@ -10,7 +11,7 @@ class AshModuleTesting(unittest.TestCase):
         self.ins1 = Ash.Enc(message=self.message1, mainkey=self.mainkey)
         self.string_message = self.ins1.encToStr()
         self.bytes_message = self.ins1.encToBytes()
-        self.ins2 = Ash.Dec(message=self.bytes_message,mainkey=self.mainkey)
+        self.ins2 = Ash.Dec(message=self.bytes_message, mainkey=self.mainkey)
 
     def tearDown(self) -> None:
         pass
@@ -19,7 +20,7 @@ class AshModuleTesting(unittest.TestCase):
         self.assertEqual(64, bytes.fromhex(Ash.Enc.genMainkey()).__len__())
 
     def test_KeyType(self):
-        self.assertIs(str,type(Ash.Enc.genMainkey()))
+        self.assertIs(str, type(Ash.Enc.genMainkey()))
 
     def test_HMAC(self):
         self.assertTrue(self.bytes_message[:64] == self.ins1.HMAC())
@@ -34,22 +35,23 @@ class AshModuleTesting(unittest.TestCase):
         self.assertTrue(self.bytes_message[96:112] == self.ins1.pepper)
 
     def test_Iterations(self):
-        self.assertTrue(self.bytes_message[112:116] == self.ins1.setupIterations())
+        self.assertTrue(
+            self.bytes_message[112:116] == self.ins1.setupIterations())
 
     def test_Ciphertext(self):
         self.assertTrue(self.bytes_message[116:] == self.ins1.ciphertext())
 
     def test_TypeIterations(self):
-        self.assertIs(bytes,type(self.ins1.setupIterations()))
+        self.assertIs(bytes, type(self.ins1.setupIterations()))
 
     def test_IterationsFixed_size(self):
-        self.assertEqual(4,self.ins1.setupIterations().__len__())
+        self.assertEqual(4, self.ins1.setupIterations().__len__())
 
     def test_EncOutputBytes(self):
-        self.assertIs(bytes,type(self.ins1.encToBytes()))
+        self.assertIs(bytes, type(self.ins1.encToBytes()))
 
     def test_EncOutputString(self):
-        self.assertIs(str,type(self.ins1.encToStr()))
+        self.assertIs(str, type(self.ins1.encToStr()))
 
     def test_HMAC_Comp(self):
         self.assertEqual(self.ins1.HMAC(), self.ins2.rec_hmac)
@@ -67,27 +69,25 @@ class AshModuleTesting(unittest.TestCase):
         self.assertEqual(self.ins1.iterations, self.ins2.rec_iterations)
 
     def test_Ciphertext_Comp(self):
-        self.assertEqual(self.ins1.ciphertext(),self.ins2.rec_ciphertext)
+        self.assertEqual(self.ins1.ciphertext(), self.ins2.rec_ciphertext)
 
     def test_HMAC_MismatchError(self):
         tampered_HMAC = self.ins1.encToBytes()[:63] + b'1'
         tampered_message = tampered_HMAC + self.ins1.encToBytes()[64:]
         with self.assertRaises(Ash.MessageTamperingError):
-            Ash.Dec(message = tampered_message, mainkey = self.mainkey)
+            Ash.Dec(message=tampered_message, mainkey=self.mainkey)
 
     def test_IterationsOutOfRangeError2(self):
         enb = self.ins1.encToBytes()
-        tampered_message = enb[:112] + struct.pack('!I',100001) + enb[116:]
+        tampered_message = enb[:112] + struct.pack('!I', 100001) + enb[116:]
         with self.assertRaises(Ash.IterationsOutofaRangeError):
-            Ash.Dec(message=tampered_message,mainkey=self.mainkey)
+            Ash.Dec(message=tampered_message, mainkey=self.mainkey)
 
     def test_DecOutputBytes(self):
         self.assertEqual(bytes, type(self.ins2.decToBytes()))
 
     def test_DecOutputString(self):
         self.assertEqual(str, type(self.ins2.decToStr()))
-
-
 
 
 if __name__ == '__main__':
